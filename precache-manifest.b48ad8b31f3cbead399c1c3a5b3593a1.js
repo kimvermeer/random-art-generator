2a6e47fc9b3820520538575d198d5c7a81105f93
Kim Vermeer <kim.e.vermeer@gmail.com>
2019-03-15 15:20:17 +0100

    Updates

---
+++ precache-manifest.b48ad8b31f3cbead399c1c3a5b3593a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "603d28602a8e27ea6601",
+    "url": "/random-art-generator/static/js/runtime~main.3bd6a7ca.js"
+  },
+  {
+    "revision": "c3c041adde7938dfee51",
+    "url": "/random-art-generator/static/js/main.c79481d4.chunk.js"
+  },
+  {
+    "revision": "ca37bcbaa78a475e4cd0",
+    "url": "/random-art-generator/static/js/2.c4c76fd2.chunk.js"
+  },
+  {
+    "revision": "c3c041adde7938dfee51",
+    "url": "/random-art-generator/static/css/main.77ea8ace.chunk.css"
+  },
+  {
+    "revision": "333ba3e874e5be5d70f39f04152179a8",
+    "url": "/random-art-generator/index.html"
+  }
+];
